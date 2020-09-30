edb9333f8d917f5a983db4d0f5f568d116157fe4
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-10-01 01:33:07 +0530

    [Siddhartha] modified: added custom Exception

---
+++ src/main/java/com/cg/ma/MoodAnalyserException.java
@@ -0,0 +1,14 @@
+package com.cg.ma;
+
+public class MoodAnalyserException extends Exception {
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY
+	}
+
+	ExceptionType type;
+
+	public MoodAnalyserException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
